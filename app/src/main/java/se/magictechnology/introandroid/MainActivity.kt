1716639ad55418ef297d016b9ae96d9cef04022c
billmartensson <bill@magictechnology.se>
2020-09-04 13:17:28 +0200

    Många utropstecken

--- app/src/main/java/se/magictechnology/introandroid/MainActivity.kt
+++ app/src/main/java/se/magictechnology/introandroid/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Nu har vi ändrat
-        // Mer ändring av text
+        // Nu har vi ändrat!!!!!
+        // Mer ändring av text!!!!
     }
 }
